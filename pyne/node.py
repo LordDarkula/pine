e1f370c8251c4e1b76d3ff05cff4a49e6c2bc04a
Aubhro <aubhrosengupta@gmail.com>
2016-07-24 17:05:06 -0700

    Added functional implementation of node

---
+++ pyne/node.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
+class Node:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def contains(self, elem):
+        pass
+    
+    @abstractmethod
+    def incl(self, elem):
+        pass
+
+class Empty(Node):
+    def __init__(self):
+        super(Empty, self).__init__(self)
+
+    def contains(self, elem): return False
+
+    def incl(self, elem): return NonEmpty(elem, Empty(), Empty())
+
+
+class NonEmpty(Node):
+    def __init__(self, elem, left, right):
+        self.elem = elem
+        self.left = left
+        self.right = right
+        super(NonEmpty, self).__init__(self)
+
+    def contains(self, elem):
+        if self.elem is elem: return elem
+        else: self.left.contains(elem) or self.right.contains(elem)
+
+    def incl(self, elem):
+        self.left.incl(elem)
